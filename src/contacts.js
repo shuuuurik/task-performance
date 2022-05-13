2ce1ccc66b44b559ce813dc60279ded7ce8b844a
unknown <mayorov.shurik@mail.ru>
2022-05-13 22:00:32 +0300

    Задание по оптимизации производительности

--- src/contacts.js
+++ src/contacts.js
@@ -1,9 +1,12 @@
 const contacts = document.getElementsByClassName("contacts")[0];
 const stickyHeader = document.getElementsByClassName("stickyHeader")[0];
 
-function addContacts() {
+let l = 0;
+let r = 5000;
+
+function addContacts(l,r) {
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < 50000; i++) {
+  for (let i = l; i < r; i++) {
     const child = document.createElement("div");
     child.textContent = i;
     child.classList.add("contact");
@@ -12,15 +15,23 @@ function addContacts() {
   contacts.appendChild(fragment);
 }
 
-contacts.addEventListener("scroll", (e) => {
+
+contacts.addEventListener('scroll', function() {
   const items = Array.from(contacts.getElementsByClassName("contact"));
   const itemOffsets = items.map((item) => item.offsetTop);
+
   const topItemIndex = itemOffsets.findIndex(
     (offset) => contacts.scrollTop - offset <= -18
   );
   if (topItemIndex !== -1) {
     stickyHeader.textContent = items[topItemIndex].textContent;
   }
+
+  if (topItemIndex >= r - 20 && r < 50000) {
+    l += 5000;
+    r += 5000;
+    addContacts(l, r);
+ }
 });
 
-addContacts();
+addContacts(l, r);
